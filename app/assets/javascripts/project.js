98efc4a99217cb73fbf797e2dbde2e1712f10e26
mosclov <federico@grupofemal.com>
2018-04-24 09:57:48 -0700

    table show/hide fix for edit

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,3 +1,5 @@
+$('#use_dollars').val() ? $('#rates_table').show() : $('#rates_table').hide()
+
 $('#use_dollars').on('change',
   function(){
   $('#use_dollars').find('option:selected').val() != "true" ? $('#rates_table').hide() : $('#rates_table').show()
