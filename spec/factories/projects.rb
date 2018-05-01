3c8e356a3e986089475c930720416928f7da0dd2
mosclov <federico@grupofemal.com>
2018-05-01 13:26:16 +0200

    fix for past tests to pass

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     sequence(:name) { |n| "project#{n}" }
     billable false
     archived false
+    use_dollars false
 
     factory :project_with_hours do
       after(:create) do |project, _evaluator|
